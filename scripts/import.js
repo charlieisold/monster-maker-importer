b0a93a546f637f89114817616a7290d65714057c
Mark Lang <marklangvt@gmail.com>
2020-07-11 10:07:03 -0400

    First test of actor creation

--- scripts/import.js
+++ scripts/import.js
@@ -1,2 +1,13 @@
-let actorData = {name: "John Doe", type: "character", img: "icons/mystery-man.png"};
-let actor = new Actor(actorData);
+// let actorData = {name: "John Doe", type: "character", img: "icons/mystery-man.png"};
+// let actor = new Actor(actorData);
+let actor = await actor.create({
+    name: "Test MacTest",
+    type: "npc",
+    img: "icons/mystery-man.png",
+    folder: folder.data_id,
+    sort: 12000,
+    data: {},
+    tokens: {},
+    items: [],
+    flags: {}
+});
